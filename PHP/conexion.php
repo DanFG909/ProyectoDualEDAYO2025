001fc87b88a62c0479258980915fc9feadd8eb39
DanF909 <danielfg788@gmail.com>
2025-10-08 14:59:26 -0600

    copiar clase conexión a carpeta PHP

---
+++ PHP/conexion.php
@@ -0,0 +1,10 @@
+<?php 
+    $conexion = new mysqli("localhost","root","","t_noviembre");
+
+    if($conexion) {
+       echo "Conectado :D"; 
+    }else {
+        echo "No se encontró la base de datos"; 
+    }
+    //cambienle el nombre :,3
+?>
